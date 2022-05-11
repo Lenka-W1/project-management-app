0d2bd48c266526c50b580c2b51a026981c2a0a24
lenka-w1 <lenka-w1@mail.ru>
2022-05-11 22:45:54 +0300

    feat: add a WelcomePage

--- src/pages/Welcome/WelcomePage.tsx
+++ src/pages/Welcome/WelcomePage.tsx
@@ -1,11 +1,39 @@
-import React from 'react';
+import { Button, Stack } from '@mui/material';
+import styled from 'styled-components';
 
 function WelcomePage() {
   return (
-    <div>
-      <h1>Welcome page</h1>
-    </div>
+    <RootWelcomeContainer>
+      <WelcomePageWrapper>
+        <ButtonBlock spacing={2} direction={'row'}>
+          <Button variant={'contained'} style={{ color: 'white' }}>
+            Sign In
+          </Button>
+          <Button variant={'contained'} style={{ color: 'white' }}>
+            Sign Up
+          </Button>
+          <Button variant={'contained'} style={{ color: 'white', display: 'none' }}>
+            Go to Main Page
+          </Button>
+        </ButtonBlock>
+      </WelcomePageWrapper>
+    </RootWelcomeContainer>
   );
 }
 
 export default WelcomePage;
+
+const RootWelcomeContainer = styled.div`
+  height: calc(100vh - 70px);
+`;
+
+const WelcomePageWrapper = styled.div`
+  margin: 0 auto;
+  max-width: 1920px;
+  padding: 20px 20px 0;
+`;
+
+const ButtonBlock = styled(Stack)`
+  display: flex;
+  justify-content: flex-end;
+`;
