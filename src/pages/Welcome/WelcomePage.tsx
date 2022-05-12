f8de28290b80adfc2583bed036c4c76db3ef9d77
lenka-w1 <lenka-w1@mail.ru>
2022-05-12 23:05:44 +0300

    feat: add a description for the app and add TeamBlock to the WelcomePage

--- src/pages/Welcome/WelcomePage.tsx
+++ src/pages/Welcome/WelcomePage.tsx
@@ -1,11 +1,170 @@
-import React from 'react';
+import { Button, Stack } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
 
 function WelcomePage() {
+  const link = useNavigate();
   return (
-    <div>
-      <h1>Welcome page</h1>
-    </div>
+    <RootWelcomeContainer>
+      <WelcomePageWrapper>
+        <TitleBlock>
+          <Title>Project managment app</Title>
+          <Stack spacing={2} direction={'row'}>
+            <Button
+              variant={'outlined'}
+              style={{ fontWeight: '600' }}
+              onClick={() => link('/sign-in')}
+            >
+              Sign In
+            </Button>
+            <Button
+              variant={'contained'}
+              style={{ color: 'white' }}
+              onClick={() => link('/sign-up')}
+            >
+              Sign Up
+            </Button>
+            <Button
+              variant={'contained'}
+              style={{ color: 'white', display: 'none' }}
+              onClick={() => link('/')}
+            >
+              Go to Main Page
+            </Button>
+          </Stack>
+        </TitleBlock>
+        <Section>
+          <Description>
+            <strong>A project management system</strong> is an application that helps an individual
+            in a team or group of developers achieve their goals.
+          </Description>
+          <WelcomeImage></WelcomeImage>
+        </Section>
+        <Section>
+          <BoardImage></BoardImage>
+          <DescriptionBoard>
+            <strong>Board</strong> use columns and cards to create tasks, track and organize them.
+            And also assign tasks to team members. Manage projects.
+          </DescriptionBoard>
+        </Section>
+        <TeamBlock>
+          <TitleTeamBlock>Application development team</TitleTeamBlock>
+          <TeamMembers>
+            <div className="developer-info">
+              <img src="photo_2022-02-19_13-11-57.jpg" className="photo two" alt="Elena" />
+              <h2>Elena Khovavko</h2>
+              <h3>Frontend developer, Team lead</h3>
+              <p></p>
+            </div>
+            <div className="developer-info">
+              <img src="photo_2022-01-06_17-44-22.jpg" className="photo one" alt="Alexander" />
+              <h2>Alexander Demidovich</h2>
+              <h3>Frontend developer</h3>
+              <p></p>
+            </div>
+          </TeamMembers>
+        </TeamBlock>
+      </WelcomePageWrapper>
+    </RootWelcomeContainer>
   );
 }
 
 export default WelcomePage;
+
+const RootWelcomeContainer = styled.div`
+  min-height: calc(100vh - 134px);
+  background: rgb(129, 200, 240);
+  background: linear-gradient(180deg, rgba(129, 200, 240, 1) 0%, rgba(234, 239, 245, 1) 69%);
+`;
+
+const WelcomePageWrapper = styled.div`
+  margin: 0 auto;
+  max-width: 1920px;
+  padding: 20px 70px 100px;
+`;
+
+const TitleBlock = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Title = styled.h1`
+  color: #091e42;
+  font-size: 24px;
+  text-transform: uppercase;
+`;
+
+const Section = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-around;
+  flex-wrap: wrap;
+  padding-top: 100px;
+`;
+
+const Description = styled.p`
+  font-size: 40px;
+  width: 700px;
+  align-self: center;
+  color: #091e42;
+  font-family: Open Sans;
+  font-weight: 500;
+  display: flex;
+  flex-direction: column;
+`;
+
+const DescriptionBoard = styled.p`
+  font-size: 40px;
+  width: 700px;
+  align-self: center;
+  color: #091e42;
+  font-family: Open Sans;
+  font-weight: 500;
+  display: flex;
+  flex-direction: column;
+`;
+
+const WelcomeImage = styled.div`
+  background-image: url(urban-teamwork.png);
+  background-repeat: no-repeat;
+  background-size: contain;
+  background-position: center;
+  width: 600px;
+  height: 500px;
+`;
+
+const BoardImage = styled.div`
+  background-image: url(urban-women-create-a-website-using-a-laptop-and-a-desktop-computer.png);
+  background-repeat: no-repeat;
+  background-size: contain;
+  background-position: center;
+  width: 600px;
+  height: 500px;
+`;
+
+const TeamBlock = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  border-top: 1px solid #1976d2;
+  padding: 80px 0;
+  margin-top: 100px;
+`;
+
+const TitleTeamBlock = styled.h2`
+  color: #1976d2;
+  font-size: 28px;
+  text-transform: uppercase;
+  margin-bottom: 20px;
+`;
+
+const TeamMembers = styled.div`
+  width: 700px;
+  min-height: 400px;
+  border-radius: 10px;
+  justify-content: space-around;
+  display: flex;
+  flex-wrap: wrap;
+  box-shadow: 0px 5px 10px 2px rgba(25, 118, 210, 0.2);
+`;
